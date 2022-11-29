385edda78a503e9b375fdc9615a6ee0eb5566944
EmilienGad <118448570+EmilienGad@users.noreply.github.com>
2022-11-29 19:59:19 +0100

    Update model.py

--- model.py
+++ model.py
@@ -25,10 +25,10 @@ class Net(nn.Module):
 
 
 
-class ResNet18(nn.Module):
+class ResNet50(nn.Module):
     def __init__(self):
-        super(ResNet18, self).__init__()
-        self.model = models.resnet18(pretrained=False)
+        super(ResNet50, self).__init__()
+        self.model = models.resnet50(pretrained=True)
         self.num_ftrs = self.model.fc.in_features
         self.model.fc = nn.Linear(self.num_ftrs, nclasses)
 
